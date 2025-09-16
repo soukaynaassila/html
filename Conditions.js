3d7bbcbc79573a116e2ebdbff782fe027c1053be
soukaynaassila <assilasoukayna@gmail.com>
2025-09-16 16:40:22 +0100

    Conditions.js

---
+++ Conditions.js
@@ -0,0 +1,12 @@
+// Conditions et logique booléenne
+
+let age = 20;
+
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
+//le reponse:Vous êtes majeur.
