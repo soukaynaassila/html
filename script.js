0d6f194ac23e62192bb509f4e950294942f2f107
soukaynaassila <assilasoukayna@gmail.com>
2025-09-16 13:26:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let nombre1 = 10;
-let nombre2 = 5;
-let somme = nombre1 + nombre2;
-console.log("La somme est : " + somme);
 
+function addition(a, b) {
+    return a + b;
+  }
+  
+  let somme2 = addition(8, 5);
+  console.log("La somme est : " + somme2);
