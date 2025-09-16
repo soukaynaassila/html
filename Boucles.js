d1190d0850e048c395078cdcd9d66734d5841f17
soukaynaassila <assilasoukayna@gmail.com>
2025-09-16 16:59:04 +0100

    Boucles.js

---
+++ Boucles.js
@@ -0,0 +1,17 @@
+// Boucles : for, while, break
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) continue; // sauter le 5
+  console.log("Nombre : " + i);
+
+  
+// REPONSE:Nombre : 1
+// script.js:3 Nombre : 2
+// script.js:3 Nombre : 3
+// script.js:3 Nombre : 4
+// script.js:3 Nombre : 6
+// script.js:3 Nombre : 7
+// script.js:3 Nombre : 8
+// script.js:3 Nombre : 9
+// script.js:3 Nombre : 10
+  // 5 IL N'as pas ecrit parce que i=5
