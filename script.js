3412290399b0e1c1bc8a6378dad225fe9cc14526
soukaynaassila <assilasoukayna@gmail.com>
2025-09-16 13:44:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let age = 20;
 
-function addition(a, b) {
-    return a + b;
-  }
-  
-  let somme2 = addition(8, 5);
-  console.log("La somme est : " + somme2);
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
+
